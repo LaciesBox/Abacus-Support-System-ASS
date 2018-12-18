24568ca5d64a10983c845b9caedfe2bc838518c2
the-sea-EXCALIBUR <nathan.bautista@obf.ateneo.edu>
2018-12-18 20:25:16 +0800

    Initial Quasar commit.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,56 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import 'quasar-extras/fontawesome'
+import 'quasar-extras/ionicons'
+import 'quasar-extras/mdi'
+import {
+  Quasar, 
+  QBtn,
+  QLayout,
+  QLayoutHeader,
+  QLayoutDrawer,
+  QPage,
+  QPageContainer,
+  QToolbar,
+  QToolbarTitle,
+  QList,
+  QListHeader,
+  QItemSeparator,
+  QItem,
+  QItemSide,
+  QItemMain,
+} from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QBtn,
+    QLayout,
+    QLayoutHeader,
+    QLayoutDrawer,
+    QPage,
+    QPageContainer,
+    QToolbar,
+    QToolbarTitle,
+    QList,
+    QListHeader,
+    QItemSeparator,
+    QItem,
+    QItemSide,
+    QItemMain,
+  },
+  directives: {
+  },
+  plugins: {
+  }
+ })
+
 Vue.config.productionTip = false
 
 new Vue({
