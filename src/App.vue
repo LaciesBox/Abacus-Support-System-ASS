f0407a51f4328e5c806839495d0b12984379f980
Cobi Bautista <nathan.bautista@orangeandbronze.com>
2018-12-19 08:42:29 +0800

    Refactored.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
   <q-layout id="q-app" view="lHh Lpr lFf">
     <toolbar />
-    
-
     <q-page-container>
-      <HelloWorld />
+      <MainPage />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
 import { openURL } from 'quasar'
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage.vue'
 import Toolbar from './components/Toolbar.vue'
 
 export default {
   name: 'LayoutDefault',
   components: {
-    HelloWorld,
+    MainPage,
     Toolbar
-  },
-  data () {
-    return {
-      leftDrawerOpen: this.$q.platform.is.desktop
-    }
   },
   methods: {
     openURL
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -12,6 +12,6 @@
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'MainPage'
 }
 </script>
