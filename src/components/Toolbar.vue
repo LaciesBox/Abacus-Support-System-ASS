ff8b266886be908d8e27bb0a7eec181d70462776
Cobi Bautista <nathan.bautista@orangeandbronze.com>
2018-12-20 07:38:14 +0800

    Added functionality for data retrieval from Google spreadsheet.

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -31,7 +31,7 @@ export default {
   },
   data () {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: this.$q.platform.is.desktop
     }
   }
   
--- src/components/screens/Calculator.vue
+++ src/components/screens/Calculator.vue
@@ -8,7 +8,31 @@
 
 <script>
 export default {
-  name: 'Calculator'
+  name: 'Calculator',
+  data () {
+    return {
+      dataEntries:null
+    }
+  },
+  watch: {
+      currentPage: 'fetchData'
+  },
+  created: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      var sheetUrl = 'https://spreadsheets.google.com/feeds/list/1ajKZbQWjKsu5eFAZ1vTRh-jamurXSub0z7NsX3ur4t8/1/public/values?alt=json'
+      var xhr = new XMLHttpRequest()
+      var self = this
+      xhr.open('GET', sheetUrl )
+      xhr.onload = function () {        
+        self.dataEntries = JSON.parse(xhr.responseText)
+        self.dataEntries = self.dataEntries.feed.entry
+      }
+      xhr.send()
+    }
+  }
 }
 </script>
 
